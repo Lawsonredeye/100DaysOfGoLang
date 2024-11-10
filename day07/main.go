fe031a043558514193a0827f8ca5abc5bb48be1c
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-11-10 19:32:01 +0100

    (mod): tested post request

--- day07/main.go
+++ day07/main.go
@@ -16,22 +16,43 @@ func CheckError(err error) {
 
 func main() {
 	// create the url
-	url := "https://jsonplaceholder.typicode.com/posts/1"
-	request, err := http.NewRequest("GET", url, nil)
+	// url := "https://jsonplaceholder.typicode.com/posts/1"
+	// request, err := http.NewRequest("GET", url, nil)
+	// CheckError(err)
+	// // create the client which would send the request
+	// client := &http.Client{}
+	// // send the get request
+	// response, err := client.Do(request)
+	// fmt.Println(response.Status)
+	// CheckError(err)
+	// // get the data and convert from a byte into a byte slice
+	// resBody, err := io.ReadAll(response.Body)
+	// CheckError(err)
+	// // printout the data from from the response
+	// var b bytes.Buffer
+	// err = json.Indent(&b, resBody, "", "  ")
+	// CheckError(err)
+	// fmt.Printf("%+v\n", string(b.Bytes()))
+	// defer response.Body.Close()
+
+	res, err := http.Get("https://jsonplaceholder.typicode.com/posts/1")
 	CheckError(err)
-	// create the client which would send the request
-	client := &http.Client{}
-	// send the get request
-	response, err := client.Do(request)
-	fmt.Println(response.Status)
+	body, err := io.ReadAll(res.Body)
 	CheckError(err)
-	// get the data and convert from a byte into a byte slice
-	resBody, err := io.ReadAll(response.Body)
+	sb := string(body)
+	fmt.Printf(sb)
+	fmt.Println()
+	defer res.Body.Close()
+
+	// sending post request
+	data, err := json.Marshal(map[string]string{
+		"name": "Lawsonredeye",
+		"email": "lawson@mail.com",
+	})
 	CheckError(err)
-	// printout the data from from the response
-	var b bytes.Buffer
-	err = json.Indent(&b, resBody, "", "  ")
+	dataBuffer := bytes.NewBuffer(data)
+	// sending post data
+	res, err = http.Post("https://postman-echo.com/post", "application/json",dataBuffer)
 	CheckError(err)
-	fmt.Printf("%+v\n", string(b.Bytes()))
-	defer response.Body.Close()
+	defer res.Body.Close()
 }
