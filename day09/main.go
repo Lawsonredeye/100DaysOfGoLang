faab5a5f8216e8a14d6f93e3f7bde19756135194
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-11-12 14:55:38 +0100

    (mod): using an interface of a type map[string] to hold the parsed data of a byte which was decoded from a json string

--- day09/main.go
+++ day09/main.go
@@ -1,21 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 )
 
 
 func main() {
-	resp, err := http.Get("https://httpbin.org/get")
-	checkErr(err)
+	// url.Values => map[string][]string
+	formData := url.Values{
+		"name": {"lawson"},
+		"address": {"332 Main Avenue, Nigeria"}, // Come visit ;)
+	}
 
+	resp, err := http.PostForm("https://httpbin.org/post", formData)
+	checkErr(err)
 	defer resp.Body.Close()
+
 	body, err := io.ReadAll(resp.Body)
 	checkErr(err)
 
-	log.Println(string(body))
+	var data map[string]interface{}
+	err = json.Unmarshal(body, &data)
+	checkErr(err)
+	log.Println(data["form"])
 }
 
 func checkErr(e error) {
