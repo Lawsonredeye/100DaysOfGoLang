c81fbd82e365899f381ed724a17a27cd67879b97
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-11-26 10:59:54 +0100

    math module with basic add & subtract function

---
+++ day23/math/add.go
@@ -0,0 +1,11 @@
+package math
+
+// Add 2 numbers together.
+func Add(leftAddend int, rightAddend int) int {
+	return leftAddend + rightAddend
+}
+
+// Subtract performs basic subraction on two operands and return the remainder.
+func Subtract(leftOperand, rightOperand int) int {
+	return leftOperand - rightOperand
+}
