150f2071ca77b33c3c59391ef7132a944743a005
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-12-04 20:14:12 +0100

    [update]: added routes to dynamic shortlinks

--- day31/url-shortner/main.go
+++ day31/url-shortner/main.go
@@ -8,6 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/create", view.CreateLink)
+	http.HandleFunc("/", view.RedirectLink)
 	log.Println("Server is running on port 8000")
 	http.ListenAndServe(":8000", nil)
 }
