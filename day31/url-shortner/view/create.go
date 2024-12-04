909c7204feb108ca754b0c54deff487c72dbd90b
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-12-04 20:13:12 +0100

    [update]: function handler for comfirming redirecting link and checking the db for shortened links

--- day31/url-shortner/view/create.go
+++ day31/url-shortner/view/create.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strings"
 	"url-shortener/controller"
 	"url-shortener/db"
 	"url-shortener/model"
@@ -34,4 +35,28 @@ func CreateLink(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.Write([]byte(link.ShortUrl))
+}
+
+func RedirectLink(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	// check the path url and then check if the shortlink exists in the database
+	// if it does then call the redirect function from the database
+	// else write to the console in correct url path.
+
+	s := r.URL.Path
+	s = strings.TrimPrefix(s, "/")
+
+	link := model.ShortLink{ShortUrl: s}
+
+	db, _ := db.ConnectDB()
+	result := db.First(&link, "short_url = ?", s)
+
+	if result.RowsAffected < 1 {
+		http.Error(w, "Bad request, check url", 400)
+		return
+	}
+	log.Printf("User successfully redirected to: %v", link.Url)
+	http.Redirect(w, r, link.Url, http.StatusFound)
+	// w.Write([]byte("Hello User"))
 }
