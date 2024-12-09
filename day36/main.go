84fdb17fef02453fa8b50aa8ebe5b11b24902da3
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-12-09 22:26:39 +0100

    channels with concurrency

--- day36/main.go
+++ day36/main.go
@@ -2,23 +2,50 @@ package main
 
 import (
 	"fmt"
-	"sync"
+	"time"
 )
 
+// func main() {
+// 	var wg sync.WaitGroup
+// 	wg.Add(2)
+// 	go helloworld(&wg)
+// 	go goodbye(&wg)
+// 	wg.Wait()
+// }
+
+// func helloworld(wg *sync.WaitGroup) {
+// 	defer wg.Done()
+// 	fmt.Println("Hello World!")
+// }
+
+// func goodbye(wg *sync.WaitGroup) {
+// 	defer wg.Done()
+// 	fmt.Println("Good Bye!")
+// }
+
 func main() {
-	var wg sync.WaitGroup
-	wg.Add(2)
-	go helloworld(&wg)
-	go goodbye(&wg)
-	wg.Wait()
+	msg := make(chan string)
+	go greet(msg)
+	time.Sleep(2 * time.Second)
+
+	greeting := <-msg
+
+	time.Sleep(2 * time.Second)
+	fmt.Println("Greeting recieved")
+	fmt.Println(greeting)
+
+	_, ok := <-msg
+	if ok {
+		fmt.Println("Channel is open!")
+	} else {
+		fmt.Println("Channel is closed!")
+	}
 }
 
-func helloworld(wg *sync.WaitGroup) {
-	defer wg.Done()
-	fmt.Println("Hello World!")
-}
+func greet(ch chan string) {
+	fmt.Println("Greeter waiting to send greetings!")
+	ch <- "Hello Docker"
+	close(ch)
 
-func goodbye(wg *sync.WaitGroup) {
-	defer wg.Done()
-	fmt.Println("Good Bye!")
+	fmt.Println("Greeter completed")
 }
