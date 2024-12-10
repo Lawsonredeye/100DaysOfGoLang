3fa0aefc891b115cd662e726ad86418b87452208
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-12-10 21:38:24 +0100

    [learning]: understanding channels and its type

---
+++ day37/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func sendOnlyChan(ch chan<- int) {
+	ch <- 10
+}
+
+func main() {
+	ch := make(chan int, 5)
+
+	for i := 0; i < 5; i++ {
+		ch <- i
+	}
+	close(ch)
+
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+	fmt.Println(cap(ch))
+	fmt.Println(<-ch)
+
+	// unidirectional channels that can only be written to and not read from
+	uniChan := make(chan<- int)
+	go sendOnlyChan(uniChan)
+
+	fmt.Println(<-uniChan) // Would panic as uniChan can only be written to and not read from
+}
