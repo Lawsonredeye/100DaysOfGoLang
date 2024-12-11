c81fb770a070b5fec1584c3bb4b0e37fcb555587
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-12-11 21:19:09 +0100

    optimizing the use of select statement

--- day38/main.go
+++ day38/main.go
@@ -2,35 +2,51 @@ package main
 
 import (
 	"fmt"
-	"sync"
-	"time"
 )
 
 func isEven(n int) bool {
 	return n%2 == 0
 }
 
+// func main() {
+// 	n := 0
+// 	var m sync.Mutex
+
+// 	go func() {
+// 		m.Lock()
+// 		defer m.Unlock()
+// 		nIsEven := isEven(n)
+
+// 		time.Sleep(5 * time.Millisecond)
+// 		if nIsEven {
+// 			fmt.Println(n, " is even")
+// 			return
+// 		}
+// 		fmt.Println(n, " is odd")
+// 	}()
+
+// 	go func() {
+// 		m.Lock()
+// 		defer m.Unlock()
+// 		n++
+// 	}()
+// 	time.Sleep(time.Second)
+// }
+
 func main() {
-	n := 0
-	var m sync.Mutex
+	bufferChan := make(chan int, 10)
 
-	go func() {
-		m.Lock()
-		defer m.Unlock()
-		nIsEven := isEven(n)
-
-		time.Sleep(5 * time.Millisecond)
-		if nIsEven {
-			fmt.Println(n, " is even")
-			return
+	for i := 0; i < 10; i++ {
+		go func() {
+			bufferChan <- i
+		}()
+	}
+	for i := 0; i < cap(bufferChan); i++ {
+		select {
+		case one := <-bufferChan:
+			fmt.Println(one)
+		case two := <-bufferChan:
+			fmt.Println(two)
 		}
-		fmt.Println(n, " is odd")
-	}()
-
-	go func() {
-		m.Lock()
-		defer m.Unlock()
-		n++
-	}()
-	time.Sleep(time.Second)
+	}
 }
