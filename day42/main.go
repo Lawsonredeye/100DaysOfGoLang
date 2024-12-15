41833ab71a3921236a819fd3bc98b696c1f8789a
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-12-15 20:57:08 +0100

    creating custom logger object in golang and setting flags with the SetFlags command

--- day42/main.go
+++ day42/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "log"
+import (
+	"log"
+	"os"
+)
 
 func main() {
 	log.Println("Hello, Logger!")
@@ -10,4 +13,11 @@ func main() {
 
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	log.Println("with file/line")
+
+	// Setting up a new custom logger object in golang
+	mylog := log.New(os.Stdout, "my:", log.LstdFlags)
+
+	// Also setting the flags for our new custom logger object using the SetFlags() method
+	mylog.SetFlags(log.LstdFlags | log.Lmicroseconds)
+	mylog.Println("using mylog logger")
 }
