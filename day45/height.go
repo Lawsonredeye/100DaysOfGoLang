8a1f8ecfcd7658a89b663ab65df8a6b7c9176dce
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-12-18 21:20:40 +0100

    [modified]: used the TreeHeight func implementation to handle the final height of the tree

--- day45/height.go
+++ day45/height.go
@@ -1,28 +1,12 @@
 package main
 
+// Calculates the tree height and return the maximum
+// nodes in the tree.
 func TreeHeight(t *Tree) int {
 	if t == nil {
 		return 0
 	}
-	// left := leftHeight(t.LeftChild)
-	// right := rightHeight(t.RightChild)
-	// fmt.Println(1 + max(left, right))
-
 	left := TreeHeight(t.LeftChild)
 	right := TreeHeight(t.RightChild)
 	return 1 + max(left, right)
-}
-
-func leftHeight(t *Tree) int {
-	if t == nil {
-		return 0
-	}
-	return 1 + leftHeight(t.LeftChild)
-}
-
-func rightHeight(t *Tree) int {
-	if t == nil {
-		return 0
-	}
-	return 1 + rightHeight(t.RightChild)
 }
