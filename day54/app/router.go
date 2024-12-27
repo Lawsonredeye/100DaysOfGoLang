51ffc5cb4201bca78d1d5f01b51f2c7dccb3612e
lawsonredeye <omoregbeeolawson@gmail.com>
2024-12-27 21:28:07 +0100

    [feat]: fetching all posts that has been added to the tasks array/slice

--- day54/app/router.go
+++ day54/app/router.go
@@ -2,10 +2,12 @@ package router
 
 import (
 	"fmt"
+	"log"
+
 	"github.com/gin-gonic/gin"
 )
 
-func CreateTask(c *gin.Context){
+func CreateTask(c *gin.Context) {
 	newTask := Task{}
 	err := c.BindJSON(&newTask)
 
@@ -14,7 +16,15 @@ func CreateTask(c *gin.Context){
 		return
 	}
 
-	fmt.Printf("%+v\n\n", newTask)
-	c.Status(201)
+	newTask.ID = len(AllTasks)
 
+	fmt.Printf("%+v task added successfully!\n\n", newTask)
+
+	AllTasks = append(AllTasks, newTask)
+	c.Status(201)
+}
+
+func GetTask(c *gin.Context) {
+	c.JSON(200, AllTasks)
+	log.Printf("%+v\n", AllTasks)
 }
