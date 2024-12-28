b401ea3a27d7e5a3b8f5e9666f8f55d225581bed
lawsonredeye <omoregbeeolawson@gmail.com>
2024-12-28 20:16:51 +0100

    [feats]: added handlers for updating a task by its id and also for deleting a task by its id

--- day55/app/router.go
+++ day55/app/router.go
@@ -3,6 +3,7 @@ package router
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
@@ -49,4 +50,44 @@ func GetTaskByID(c *gin.Context) {
 	}
 	res := map[string]string{"error": "Task not found"}
 	c.AbortWithStatusJSON(402, res)
+}
+
+// UpdateTaskByID updates stored tasks by its ID.
+func UpdateTaskByID(c *gin.Context) {
+	stringID := c.Param("id")
+	id, err := strconv.Atoi(stringID)
+	if err != nil {
+		c.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	for _, val := range AllTasks {
+		if id == val.ID {
+			newData := Task{}
+			err := c.BindJSON(newData)
+			if err != nil {
+				c.AbortWithError(http.StatusGatewayTimeout, err)
+				return
+			}
+			val.Title = newData.Title
+			val.Description = newData.Description
+			c.JSON(http.StatusOK, val)
+			return
+		}
+	}
+	c.AbortWithStatusJSON(http.StatusNotFound, map[string]string{"error": "Task with id not found"})
+}
+
+// DeleteTaskByID removes a specific task from the db in-memory storage.
+func DeleteTaskByID(c *gin.Context) {
+	paramID, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+	newTaskA := AllTasks[:paramID]
+	newTaskB := AllTasks[paramID+1:]
+	AllTasks = newTaskA
+	AllTasks = append(AllTasks, newTaskB...)
+	c.Status(http.StatusOK)
 }
