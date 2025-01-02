c57540ababb03ab2ac568db6be17424c057484ac
lawsonredeye <omoregbeeolawson@gmail.com>
2025-01-02 21:39:34 +0100

    [feature]: basic route handler for handling a welcome

---
+++ day60/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	fmt.Println("vim-go")
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Gotcha",
+		})
+	})
+
+	r.Run()
+}
