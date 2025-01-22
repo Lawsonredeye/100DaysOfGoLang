bda1a0455922d5d836a042b14c2bc5368bfbf01b
Lawsonredeye <omoregbeeolawson@gmail.com>
2025-01-22 12:19:42 +0100

    [fix]: implemented logic for checking if user email and username already exists before user account creation

--- day77/go-ecommerce/controller/UserController.go
+++ day77/go-ecommerce/controller/UserController.go
@@ -5,19 +5,32 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
 
+// CreateAccount handles the creation of a new user account.
+// Use the POST request and accept a content-type: application/json,
+// typically contain the user details e.g (username, password, email).
+// This function validates the email and username
+// Parameter:
+// - c: *gin.Context : GIN framework context manager
+// Response:
+// - HTTP 201: Account created successfully.
+// - HTTP 400: User Details already exists and user account was not created.
+// - HTTP 501: User account creation failed due to cryptograph hashing
 func CreateAccount(c *gin.Context) {
-	// check if the incoming request is a JSON application
 	if c.ContentType() != "application/json" {
-		c.AbortWithStatus(http.StatusBadRequest)
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": "Wrong Content-Type Header",
+		})
 		return
 	}
 	// create a user object for handling the json data
 	var user model.Users
-
 	c.BindJSON(&user)
 
+	// check if the following fields are empty ['username', 'email', 'password']
+	// && return 400 if empty
 	if user.Username == "" {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Empty username field."})
 		return
@@ -32,13 +45,46 @@ func CreateAccount(c *gin.Context) {
 		return
 	}
 
-	// check if the following fields are empty ['username', 'email', 'password'] && return 400 if empty
+	// check if the username and email already exists in the db
+	// && return 401 if exists
+	var resultUser model.Users
+	model.DB.Where("username = ?", user.Username).First(&resultUser)
 
-	// check if the username already exists in the db && return 401 if exists
-	model.DB.Where()
+	if resultUser.Username != "" {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": "Username already exists",
+		})
+		return
+	}
+	
+	var emailUser model.Users
+	model.DB.Where("email = ?", user.Email).First(&emailUser)
+
+	if emailUser.Email != "" {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": "Account with email already exists.",
+		})
+		return
+	}
 	// hash password using bycrypt and then write to the go user object
+	pwd := user.PasswordHash
+	newHashedPassword, err := bcrypt.GenerateFromPassword([]byte(pwd), 10)
+	user.PasswordHash = string(newHashedPassword)
 
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusNotImplemented, gin.H{
+			"error": "Password hashing failed.",
+		})
+		return
+	}
 	// using the gorm connection, save the new instance of the data and update the userId
+	result := model.DB.Create(&user)
+	if result.Error != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": "Account not created, try again.",
+		})
+		return
+	}
 
 	c.JSON(201, gin.H{
 		"message": "account creation is successful",
