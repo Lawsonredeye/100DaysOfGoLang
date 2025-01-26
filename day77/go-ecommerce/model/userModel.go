5fa7f6ac61073d0244366da426f15ee31d524a8d
Lawsonredeye <omoregbeeolawson@gmail.com>
2025-01-26 20:57:05 +0100

    [fix]: updated the struct tag from improper naming and also added the orders struct for handling the creation of new orders

--- day77/go-ecommerce/model/userModel.go
+++ day77/go-ecommerce/model/userModel.go
@@ -8,8 +8,8 @@ type Users struct {
 	Email        string    `json:"email"`
 	PasswordHash string    `json:"password"`
 	Role         string    `json:"role"`
-	Created_at   time.Time `gorm:"autoCreateTime"`
-	Updated_at   time.Time `gorm:"autoCreateTime"`
+	Created_at  time.Time `gorm:"autoCreateTime" json:"created_at"`
+	Updated_at  time.Time `gorm:"autoCreateTime" json:"updated_at"`
 }
 
 type Product struct {
@@ -17,7 +17,7 @@ type Product struct {
 	Name        string    `json:"name"`
 	Description string    `json:"description"`
 	SKU         string    `json:"sku"`
-	Category string `json:"category"`
+	Category    string    `json:"category"`
 	CategoryID  int       `json:"category_id"`
 	Color       string    `json:"color"`
 	ProductSize int       `json:"product_size"`
@@ -28,17 +28,26 @@ type Product struct {
 }
 
 type Categories struct {
-	ID uint `json:"int"`
-	Name string `json:"string"`
-	Abbrevation string `json:"string"`
+	ID          uint   `json:"id"`
+	Name        string `json:"name"`
+	Abbrevation string `json:"abbrevation"`
+}
+
+type Orders struct {
+	ID          uint      `json:"id"`
+	UserID      uint      `json:"user_id"`
+	TotalAmount float64   `json:"total_amount"`
+	Status      string    `json:"status"`
+	Created_at  time.Time `gorm:"autoCreateTime" json:"created_at"`
+	Updated_at  time.Time `gorm:"autoCreateTime" json:"updated_at"`
 }
 
 type OrderItem struct {
-	ID uint `json:"id"`
-	OrderID int `json:"order_id"`
-	ProductID uint 	`json:"product_id"`
-	Quantity int 	`json:"quantity"`
-	Price	float64 `json:"price"`
-	Created_at  time.Time `gorm:"autoCreateTime" json:"created_at"`
-	Updated_at  time.Time `gorm:"autoCreateTime" json:"updated_at"`
+	ID         uint      `json:"id"`
+	OrderID    int       `json:"order_id"`
+	ProductID  uint      `json:"product_id"`
+	Quantity   int       `json:"quantity"`
+	Price      float64   `json:"price"`
+	Created_at time.Time `gorm:"autoCreateTime" json:"created_at"`
+	Updated_at time.Time `gorm:"autoCreateTime" json:"updated_at"`
 }
