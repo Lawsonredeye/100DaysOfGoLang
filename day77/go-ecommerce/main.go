89a8c3193872d875c878fb50dd58320a0bc740b5
Lawsonredeye <omoregbeeolawson@gmail.com>
2025-01-26 22:35:32 +0100

    [feat]: added versioning to route

--- day77/go-ecommerce/main.go
+++ day77/go-ecommerce/main.go
@@ -12,13 +12,13 @@ func main() {
 
 	model.DB.AutoMigrate(&model.Users{})
 
-	r.POST("/signup", controller.CreateAccount)
-	r.POST("/login", controller.Login)
-	r.GET("/logout", controller.Logout)
-	r.POST("/product", controller.AddProduct)
-	r.GET("/product/:name", controller.FindProductByName)
-	r.DELETE("/product/:id", controller.DeleteProductByID)
-	r.PUT("/product/:id", controller.UpdateProductByID)
-	r.POST("/orders", controller.AddOrder)
+	r.POST("/api/v1/signup", controller.CreateAccount)
+	r.POST("/api/v1/login", controller.Login)
+	r.GET("/api/v1/logout", controller.Logout)
+	r.POST("/api/v1/product", controller.AddProduct)
+	r.GET("/api/v1/product/:name", controller.FindProductByName)
+	r.DELETE("/api/v1/product/:id", controller.DeleteProductByID)
+	r.PUT("/api/v1/product/:id", controller.UpdateProductByID)
+	r.POST("/api/v1/orders", controller.AddOrder)
 	r.Run(":5050")
 }
