2dca98aaecc8d9718598d35c65aad2c501928936
Lawsonredeye <omoregbeeolawson@gmail.com>
2025-02-06 21:17:24 +0100

    [docs]: using a new object by updating the current object when working with useState is much more better as its one of the rules of react

--- day93/vite-project/src/main.jsx
+++ day93/vite-project/src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {App} from './welcome'
+import {Person} from './welcome'
 // import { LiveMsg } from './welcome'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Person />
     {/* <LiveMsg headerMsg = "Hello, Lawson"/> */}
   </StrictMode>,
 )
--- day93/vite-project/src/welcome.jsx
+++ day93/vite-project/src/welcome.jsx
@@ -10,7 +10,7 @@ export function App() {
     };
 
     return (
-        <>
+        <div style={{"background-color": color }}>
             <h1 style={{color}}>Hello, World</h1>
             <button type="button" style={{color}}>Click here</button>
             {COLORS.map((color) => (
@@ -20,7 +20,32 @@ export function App() {
                     Click, Me
                 </button>
             ))}
-        </>
+        </div>
     )
 
+}
+
+// When working with States like array and objects its good to copy the object into a new object while updating the property
+export function Person() {
+    const [person, setPerson] = useState({ name: "Lawson", age: 100 });
+    
+    const handleIncreaseAge = () => {
+        const newPerson = { ...person, age: person.age + 1 };
+        setPerson(newPerson);
+    }
+
+    const handleDecreaseAge = () => {
+        const newPerson = { ...person, age: person.age - 1 };
+        setPerson(newPerson);
+    };
+
+    return (
+        <>
+            <h1>{person.name}</h1>
+            <h2>{person.age}</h2>
+            <button onClick={handleIncreaseAge}>Increase age</button>
+            <br />
+            <button onClick={handleDecreaseAge}>Decrease age</button>
+        </>
+    );
 }
