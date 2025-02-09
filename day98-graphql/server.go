a51959009d72e457c5be67dc26bf1b7f4c6fa535
Lawsonredeye <omoregbeeolawson@gmail.com>
2025-02-09 23:01:00 +0100

    [feat]
    Implemented the complete logic for a graphql but it doesnt fetch data even while the mutation or query has been passed

--- day98-graphql/server.go
+++ day98-graphql/server.go
@@ -1,17 +1,16 @@
 package main
 
 import (
+	"database/sql"
 	"go-graphql/graph"
+	"go-graphql/internal/database"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/handler/extension"
-	"github.com/99designs/gqlgen/graphql/handler/lru"
-	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/vektah/gqlparser/v2/ast"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 const defaultPort = "8080"
@@ -22,18 +21,27 @@ func main() {
 		port = defaultPort
 	}
 
-	srv := handler.New(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	categoryDb := database.NewCategory(db)
 
-	srv.AddTransport(transport.Options{})
-	srv.AddTransport(transport.GET{})
-	srv.AddTransport(transport.POST{})
+	srv := handler.New(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+		CategoryDB: categoryDb,
+	}}))
 
-	srv.SetQueryCache(lru.New[*ast.QueryDocument](1000))
+	// srv.AddTransport(transport.Options{})
+	// srv.AddTransport(transport.GET{})
+	// srv.AddTransport(transport.POST{})
 
-	srv.Use(extension.Introspection{})
-	srv.Use(extension.AutomaticPersistedQuery{
-		Cache: lru.New[string](100),
-	})
+	// srv.SetQueryCache(lru.New[*ast.QueryDocument](1000))
+
+	// srv.Use(extension.Introspection{})
+	// srv.Use(extension.AutomaticPersistedQuery{
+	// 	Cache: lru.New[string](100),
+	// })
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
